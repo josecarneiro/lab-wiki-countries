9fa64c7a5ecddb44857450898d205d8b2e762b1a
Ric Az <ricardoazevedo96@gmail.com>
2020-03-09 15:33:39 +0000

    almoste

---
+++ src/components/countries/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+export class index extends Component {
+  render() {
+    const { countrie } = this.props;
+  
+    return (
+      <div>
+      <Link className="list-group-item list-group-item-action" to={`/${countrie.cca2}`} >
+      
+      <h5>{countrie.flag}</h5> <h5 >{countrie.name.official}</h5>
+      </Link> 
+        
+      </div>
+    )
+  }
+}
+
+export default index
