adfd0ecf52e7071d71d76bc9160fc8e22faaa1e3
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-03-09 21:31:05 +0000

    lab done

--- src/components/Borders/index.jsx
+++ src/components/Borders/index.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import Countries from '../../countries.json';
+import { Link } from 'react-router-dom';
+
+
 
 export default class Borders extends Component {
+ 
   render() {
-    const borders = this.props.borders;
+    let arrayCountry = [];
+
+    const borderCountry = this.props.borders;
+
+    for(let i=0;i<borderCountry.length;i++){
+      arrayCountry.push(Countries.filter(country => country.cca3 === borderCountry[i]));
+    }
+    
+   
     return (
       <ul>
-        {
-          borders.map(country => <li>{country}</li>)
-        }
+        {arrayCountry.map( country => 
+          <li>
+            <Link to={country[0].cca3}>
+              {country[0].name.common}
+            </Link>
+          </li>
+
+       )}
       </ul>
     )
   }
